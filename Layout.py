800991e6d4f4846f7136c4984060a7d992e0548f
J.J.Ding <louis7012161@gmail.com>
2020-03-01 17:33:57 +0800

    修改TreeView欄位格式 新增關閉視窗的MessageBox

--- Layout.py
+++ Layout.py
@@ -1,34 +1,55 @@
 #!evn01/Scripts/python3.8
 """ docString"""
 from tkinter import *
-from tkinter import ttk
+from tkinter.ttk import *
+from tkinter import messagebox
+
 
 def readText():
     print('button click')
 
+
+def closeWindow(): #關閉視窗的MessgaeBox function
+    ans=messagebox.askyesno('關閉視窗', '是否離開?')
+    if ans == True:
+        window.destroy()
+    else:
+        return
+
 def interface(w):
     """ 建立介面 """
-    w.option_add("*font",('verdana',16))
-    w.title("體溫量測") #視窗標題
-    w.geometry("900x600") #視窗大小
+    w.option_add("*font", ('verdana', 16))
+    w.title("體溫量測")  # 視窗標題
+    w.geometry("520x420")  # 視窗大小
+
+    name = Label(w, text='姓名:').place(x=10, y=10)
+    temp = Label(w, text='體溫:').place(x=160, y=10)
+    upload = Button(w, text='上傳').place(x=420, y=15)
     w.wm_resizable(height=False, width=False)
 
-    Label( text='姓名').pack()
-    Label( text='體溫').pack()
-    Label(text='量測數據').pack()
-    Button(text='上傳').pack()
+    '''建立區域擺放清單'''
+    labFrame = LabelFrame(w, text='量測數據')
+    tree = Treeview(labFrame, columns=('ID', '姓名', '體溫', '狀態'), show='headings')
+    tree.column('ID', width=40)
+    tree.column('姓名', width=40)
+    tree.column('體溫', width=40)
+    tree.column('狀態', width=40)
+    tree.heading('ID', text='ID')
+    tree.heading('姓名', text='姓名')
+    tree.heading('體溫', text='體溫')
+    tree.heading('狀態', text='狀態')
+    tree.place(x=10, y=10, width=480, height=320)
+    labFrame.place(x=10, y=50, width=500, height=360)
 
-    '''建立區域擺放清單(待處理)'''
-    listView = Frame(window,borderwidth=100, relief=GROOVE)
-    tree = ttk.Treeview(listView, columns=('col1', 'col2', 'col3'))
-    tree.column('col1', width=100, anchor='center')
-    tree.column('col2', width=100, anchor='center')
-    tree.column('col3', width=100, anchor='center')
-    tree.heading('col1', text='col1')
-    tree.heading('col2', text='col2')
-    tree.heading('col3', text='col3')
+    yscrollbar = Scrollbar(tree)  # y軸scroller物件
+    yscrollbar.pack(side=RIGHT, fill=Y)  # y軸scroller包裝顯示
+    yscrollbar.config(command=tree.yview)  # y軸scroller
+
+
+
 
 if __name__ == "__main__":
     window = Tk()
+    window.protocol('WM_DELETE_WINDOW', closeWindow) #關閉視窗的MessgaeBox
     interface(w=window)
     window.mainloop()
